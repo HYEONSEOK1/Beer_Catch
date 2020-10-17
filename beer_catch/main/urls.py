7604e131890aef9e258bd6d1eaa51fc79b700f9a
tjswn9727 <55373167+tjswn9727@users.noreply.github.com>
2020-10-18 00:42:23 +0900

    Add Social Login

--- beer_catch/main/urls.py
+++ beer_catch/main/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', api_views.index, name="index"),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
