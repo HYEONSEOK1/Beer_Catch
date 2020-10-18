4d010a398bf5f89ea9b60eeebc53e901f2c178f8
tjswn9727 <55373167+tjswn9727@users.noreply.github.com>
2020-10-19 01:22:13 +0900

    Modify User Fields

--- beer_catch/api/models.py
+++ beer_catch/api/models.py
@@ -8,9 +8,11 @@ class ImageUpload(models.Model):
 class User(models.Model):
     user_id = models.CharField(max_length=100, unique=True)
     name = models.CharField(max_length=20)
+    nickname = models.CharField(max_length=20, unique=True)
     email = models.CharField(max_length=100)
     gender = models.CharField(max_length=20)
     type = models.CharField(max_length=20)
+    beer = models.CharField(max_length=5000)
 
     class Meta:
         db_table = "User"
