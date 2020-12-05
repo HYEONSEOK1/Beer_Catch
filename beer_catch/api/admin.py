fca219fa0e825b2c3cae00416534317513e23fa8
tjswn9727 <55373167+tjswn9727@users.noreply.github.com>
2020-12-05 18:49:30 +0900

    Update admin.py

--- beer_catch/api/admin.py
+++ beer_catch/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from api.models import ImageUpload, User, Beer, Review, BeerLike, ReviewLike, Ingredient
 # Register your models here.
+admin.site.register(ImageUpload)
+admin.site.register(User)
+admin.site.register(Beer)
+admin.site.register(Review)
+admin.site.register(BeerLike)
+admin.site.register(ReviewLike)
+admin.site.register(Ingredient)
